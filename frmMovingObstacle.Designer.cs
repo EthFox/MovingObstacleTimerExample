b5438bcebae39777f884105af272d50bf35cb751
EthFox <168486262+EthFox@users.noreply.github.com>
2024-04-30 18:16:48 -0400

    Add project files.

---
+++ frmMovingObstacle.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MovingObstacleTimerExample
+{
+	partial class frmMovingObstacle
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.movingObstacle = new System.Windows.Forms.PictureBox();
+			this.ObstacleTimer = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.movingObstacle)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// movingObstacle
+			// 
+			this.movingObstacle.BackColor = System.Drawing.Color.Red;
+			this.movingObstacle.Location = new System.Drawing.Point(100, 234);
+			this.movingObstacle.Name = "movingObstacle";
+			this.movingObstacle.Size = new System.Drawing.Size(25, 25);
+			this.movingObstacle.TabIndex = 0;
+			this.movingObstacle.TabStop = false;
+			// 
+			// ObstacleTimer
+			// 
+			this.ObstacleTimer.Enabled = true;
+			this.ObstacleTimer.Tick += new System.EventHandler(this.ObstacleTimer_Tick);
+			// 
+			// frmMovingObstacle
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(416, 382);
+			this.Controls.Add(this.movingObstacle);
+			this.Name = "frmMovingObstacle";
+			this.Text = "Moving Obstacle Example";
+			this.Load += new System.EventHandler(this.frmMovingObstacle_Load);
+			((System.ComponentModel.ISupportInitialize)(this.movingObstacle)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox movingObstacle;
+		private System.Windows.Forms.Timer ObstacleTimer;
+	}
+}
+
---
+++ frmMovingObstacle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObstacleTimerExample
+{
+	public partial class frmMovingObstacle : Form
+	{
+		public frmMovingObstacle()
+		{
+			InitializeComponent();
+		}
+
+		Point obstacleLoc;
+		bool obstacleDirection; // false = right, true = left
+
+		private void frmMovingObstacle_Load(object sender, EventArgs e)
+		{
+			obstacleLoc = movingObstacle.Location;
+		}
+
+		private void ObstacleTimer_Tick(object sender, EventArgs e) // interval property is set to 100, which means 10 times per second
+		{                                                                                      //   ^ in milliseconds
+			// Oscillates obstacle between x = 100 and 300
+			if (obstacleDirection == false) // if moving right...
+			{
+				obstacleLoc.Offset(new Point(10, 0)); // ...move 10 pixels to the right
+			}
+			else // if moving left...
+			{
+				obstacleLoc.Offset(new Point(-10, 0)); // ...move 10 pixels to the left
+			}
+
+			movingObstacle.Location = obstacleLoc;
+
+			if (obstacleLoc.X == 300) // once the obstacle reaches x = 300...
+			{
+				obstacleDirection = true; // ...start moving left
+			}
+			else if (obstacleLoc.X == 100) // once the obstacle gets back to x = 100...
+			{
+				obstacleDirection = false; // ...start moving right again
+			}
+		}
+	}
+}
